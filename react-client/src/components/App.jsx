6704649c89417e3468fe6ba264077fea5e8f1ebd
Jacob Granberry <jacobgranberry@gmail.com>
2018-02-21 17:09:25 -0600

    Attempts to toggle fields based on dropdown value

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import LoginForm from './Login.jsx';
+import Signup from './Signup.jsx'
 
 const App = ({ store }) => (
   <div>
       <Provider store={store}>
-        <LoginForm/>
+        <Signup/>
       </Provider>
   </div>
 )
--- react-client/src/components/Signup.jsx
+++ react-client/src/components/Signup.jsx
@@ -7,41 +7,80 @@ const options = [
   ]
 
 class SignUpForm extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            CandidateTrue: undefined
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange() {
+        //console.log(options)
+        for (var i = 0; i < options.length; i++) {
+            console.log(options[i])
+        }
+        if (options[0] === 'voter') {
+            this.setState({
+                CandidateTrue: true
+            })
+        } else {
+            this.setState({
+                CandidateTrue: true
+            }) 
+        }
+    }
 
 
     render() {
         return (
-    <div className='signup-form'>
-    <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
-      <Grid
-        textAlign='center'
-        style = {{ height: '100%' }}
-        verticalAlign='middle'
-      >
-        <Grid.Column style={{ maxWidth: 750 }}>
-          <Header size='huge' style={{ fontSize: 60 }}>GRASSROOTS</Header>
-          <Header as='h2' color='green' textAlign='center'>
-            {' '}Sign up for an account
-          </Header>
-  
-          <Form size='large'>
-          <Segment>
-            <Form.Group widths='equal'>
-                <Form.Field control={Input} label='First name' placeholder='First name' />
-                <Form.Field control={Input} label='Last name' placeholder='Last name' />
-                <Form.Field control={Select} label='Role' options={options} placeholder='Role' />
-            </Form.Group>
-            </Segment>
-          </Form>
-        </Grid.Column>
-      </Grid>
-    </div>
+            <div className='signup-form'>
+                <style>{`
+                body > div,
+                body > div > div,
+                body > div > div > div.login-form {
+                    height: 100%;
+                }
+                `}</style>
+                <Grid
+                    textAlign='center'
+                    style = {{ height: '100%' }}
+                    verticalAlign='middle'
+                >
+                    <Grid.Column style={{ maxWidth: 750 }}>
+                    <Header size='huge' style={{ fontSize: 60 }}>GRASSROOTS</Header>
+                    <Header as='h2' color='green' textAlign='center'>
+                        {' '}Sign up for an account
+                    </Header>
+            
+                    <Form size='large'>
+                    <Segment>
+                        <Form.Group widths='equal'>
+                            <Form.Field required control={Input} label='Username' placeholder='Username' />
+                            <Form.Field required control={Input} label='Password' placeholder='Password' />
+                            <Form.Field required control={Select} 
+                                        label='Role' 
+                                        options={options} 
+                                        placeholder='Role' 
+                                        onChange={this.handleChange}/>
+                        </Form.Group>
+
+                        { this.state.CandidateTrue && [
+                        <Form.Group widths='equal' key="1">
+                            <Form.Field key="2" control={Input} label='First Name' placeholder='First name' />
+                            <Form.Field key="3" control={Input} label='Last Name' placeholder='Last name' />
+                        </Form.Group>
+                        ]
+                        }
+
+                            <Form.Field control={Button}>Submit</Form.Field>
+                        </Segment>
+                        
+                        
+                    </Form>
+                    </Grid.Column>
+                </Grid>
+            </div>
         )
     }
 }
