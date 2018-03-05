242b271517522831e6de4a1ca3c416b42386f3a0
Sean Quillen <sean.r.quillen@gmail.com>
2018-03-05 16:34:32 -0600

    adds in comments for future understanding

--- react-client/src/components/CandidateInfoTab.jsx
+++ react-client/src/components/CandidateInfoTab.jsx
@@ -19,20 +19,20 @@ class ConnectedCandidateInfoTab extends React.Component {
   componentWillMount() {
     // create conditional to check if user is a voter--THAT INFO IS NOT CURRENTLY STORED IN CURRENTUSER INFO
     // if so
-      this.fetchCandidateInfo(this.props.saveCandidateInfo, window.localStorage.zipCode || '78701') //replace with currentUser zip (can take any address though) -- THAT INFO IS NOT CURRENTLY STORED IN CURRENTUSER INFO
+      this.fetchCandidateInfo(this.props.saveCandidateInfo, window.localStorage.zipCode || '78701'); //replace with currentUser zip (can take any address though) -- THAT INFO IS NOT CURRENTLY STORED IN CURRENTUSER INFO
     // else
       // do nothing
   }
 
   fetchCandidateInfo(dispatch, address) {
     let location = helper.encodeRequest(address);
-    axios.get(`https://www.googleapis.com/civicinfo/v2/representatives?key=${config.GOOGLE_API_KEY}&address=${location}&levels=administrativeArea1`)
+    axios.get(`https://www.googleapis.com/civicinfo/v2/representatives?key=${config.GOOGLE_API_KEY}&address=${location}&levels=administrativeArea1`)  // level is hard coded because it is the most relevant but there are several other options
       .then(function (response) {
-        dispatch(response)
-        console.log('Candidate info successfully updated', response)
+        dispatch(response);
+        console.log('Candidate info successfully updated', response);
       })
       .catch(function (error) {
-        console.log('There was an error retrieving the candidate information from the API', error)
+        console.log('There was an error retrieving the candidate information from the API', error);
       })
   }
 
@@ -55,11 +55,11 @@ class ConnectedCandidateInfoTab extends React.Component {
     }
     return (
       <div>
-        {this.props.candidateInfo.data !== undefined 
+        {this.props.candidateInfo.data !== undefined // this keeps react from rendering when object is undefined, which will crash the page
           ? <div>
               <p style={styles.header}>Representatives in { searchedLocation.city }, { searchedLocation.state }</p>
               <Grid divided='vertically'>
-                {this.props.candidateInfo.data.officials.map(candidate => ( // takes the first 10 (because there are a lot)
+                {this.props.candidateInfo.data.officials.map(candidate => (
                     <Grid.Row columns={1} key={ uuidv1() }>
                       <Grid.Column>
                         <div>
@@ -72,7 +72,7 @@ class ConnectedCandidateInfoTab extends React.Component {
                   ))}
               </Grid>
             </div> 
-          :  <div>
+          :  <div>  { /*this will only render if the candidateInfo.data object is undefined. Would be cool to have loading symbol*/ }
               <Container>
                 loading representatives
               </Container>
@@ -83,16 +83,18 @@ class ConnectedCandidateInfoTab extends React.Component {
   }
 }
 
+// Redux
 let mapDispatchToProps = dispatch => {
   return { saveCandidateInfo: results => dispatch(saveCandidateInfo(results)) };
 }
 
 let mapStateToProps = state => {
   return {
-    candidateInfo: state.data.candidateInfo,
-    currentUser: state.data.currentUser
+    candidateInfo: state.data.candidateInfo, 
+    currentUser: state.data.currentUser  // currently not being used since the currentUser does not contain much info
   };
 }
 
+// this connects the component to the store, enabled by wrapping the Root with Provider. This turns the component from a "dumb component" (unaware of the store) to a "smart component"  (aware of the store)
 const CandidateInfoTab = connect(mapStateToProps, mapDispatchToProps)(withRouter(ConnectedCandidateInfoTab));
 export default CandidateInfoTab;
