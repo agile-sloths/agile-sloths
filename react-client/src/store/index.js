333bf388e6a1528219a4983062f7d2f42cd73422
Sean Quillen <sean.r.quillen@gmail.com>
2018-03-05 17:06:13 -0600

    refactors Root component to only take one store

--- react-client/src/store/index.js
+++ react-client/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'redux';
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
 import mainReducer from '../../src/reducers/reducers.js';
 
 const reducer = combineReducers({ // combine all reducers from reducer file
-  data: mainReducer
+  data: mainReducer,
+  routing: routerReducer
 });
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); // create store and allow for chrome redux plugin
