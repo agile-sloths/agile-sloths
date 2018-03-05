89c89bbad8ccdbb889e4f0f21f11be2c56a899d7
Sean Quillen <sean.r.quillen@gmail.com>
2018-03-05 17:10:31 -0600

    adds routerReducer to combineReducers

--- react-client/src/store/index.js
+++ react-client/src/store/index.js
@@ -5,7 +5,7 @@ import mainReducer from '../../src/reducers/reducers.js';
 
 const reducer = combineReducers({ // combine all reducers from reducer file
   data: mainReducer,
-  routing: routerReducer
+  routing: routerReducer 
 });
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); // create store and allow for chrome redux plugin
